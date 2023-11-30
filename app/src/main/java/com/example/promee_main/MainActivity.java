81676ab226bff5cfee7513f7d64bc99de365cdd0
Joar <crisostomo.mavijoar2003@gmail.com>
2023-11-30 19:45:47 +0800

    Nav drawer elements adjusted

--- app/src/main/java/com/example/promee_main/MainActivity.java
+++ app/src/main/java/com/example/promee_main/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawerLayout;
         ImageView menu;
         ImageView backbtn;
-        LinearLayout profile, friends, groups, settings, help, logout;
+        LinearLayout profile, friends, groups, settings, help;
+        Button logout;
         ViewPager pager;
         PagerAdapter pagerAdapter;
 
